d9e01b2bb3a7d94fe5e1ce1c74254857ed714cf2
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2022-05-29 16:06:45 -0300

    empezando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,23 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'lab4-clinica-lucasvaldiviezo',
+    appId: '1:389068768804:web:8e1c9f70550ac842963ab1',
+    storageBucket: 'lab4-clinica-lucasvaldiviezo.appspot.com',
+    apiKey: 'AIzaSyAR99uRIbxfQWM880k867wUt2Vfwk8OhIw',
+    authDomain: 'lab4-clinica-lucasvaldiviezo.firebaseapp.com',
+    messagingSenderId: '389068768804',
+  },
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAR99uRIbxfQWM880k867wUt2Vfwk8OhIw",
+    authDomain: "lab4-clinica-lucasvaldiviezo.firebaseapp.com",
+    projectId: "lab4-clinica-lucasvaldiviezo",
+    storageBucket: "lab4-clinica-lucasvaldiviezo.appspot.com",
+    messagingSenderId: "389068768804",
+    appId: "1:389068768804:web:8e1c9f70550ac842963ab1"
+  }
 };
 
 /*
